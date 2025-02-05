50e27bc01ae99ceae2befa60029fb17214bdc747
tuyoleni <simeondevs@gmail.com>
2025-02-05 21:04:05 +0200

    route issues

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,49 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { FirestoreAdapter } from "@auth/firebase-adapter";
-import { cert } from "firebase-admin/app";
-import { Adapter, AdapterUser } from "next-auth/adapters";
-import { Session } from "next-auth";
-
-export const authOptions: NextAuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID!,
-            clientSecret: process.env.GOOGLE_SECRET!,
-        }),
-    ],
-    adapter: FirestoreAdapter({
-        credential: cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
-        })
-    }) as Adapter,
-    pages: {
-        signIn: '/register',
-        error: '/auth/error',
-    },
-    debug: process.env.NODE_ENV === 'development',
-    callbacks: {
-        async session({
-            session,
-            user
-        }: {
-            session: Session;
-            user: AdapterUser;
-        }) {
-            if (session.user) {
-                if (user.email) {
-                    session.user.email = user.email;
-                }
-                if (user.name) {
-                    session.user.name = user.name;
-                }
-            }
-            return session;
-        },
-    },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "./authOptions";
 
 const handler = NextAuth(authOptions);
 
--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "../[...nextauth]/authOptions";
 
 export async function GET() {
     const session = await getServerSession(authOptions);
