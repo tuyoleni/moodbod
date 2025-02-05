515532feb34bcf700d5e1e82df9f559a03f0b7ee
tuyoleni <simeondevs@gmail.com>
2025-02-05 20:56:49 +0200

    route issues

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../[...nextauth]/route";
 import { NextResponse } from "next/server";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export async function GET() {
     const session = await getServerSession(authOptions);
