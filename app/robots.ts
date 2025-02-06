10c3c757fd9289678d51b86e887e30d4ed78eb06
tuyoleni <simeondevs@gmail.com>
2025-02-06 22:39:06 +0200

    allowed dashboard

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
             disallow: [
-                '/dashboard/',
                 '/api/',
                 '/auth/',
             ],
