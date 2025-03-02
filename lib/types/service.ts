866bd6a87a7728b0f745ad0c8c04db16dd23ad09
tuyoleni <simeon.devs@gmail.com>
2025-03-02 02:58:31 +0200

    service type fix

--- lib/types/service.ts
+++ lib/types/service.ts
@@ -2,12 +2,10 @@ import { Project } from './project';
 import { ServiceStatus } from './enums';
 
 export interface Service {
-    requestedAt: Record<string, unknown>;
     id: string;
     name: string;
     description: string;
-    projectId: string;
-    project?: Project;
-    createdAt: Date;
     status: ServiceStatus;
+    category: string;
+    price: number;
 }
