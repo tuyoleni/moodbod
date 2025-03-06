1f2f69d32cfb03f29cb63d2f1e65f92425516d26
tuyoleni <simeon.devs@gmail.com>
2025-03-06 14:46:41 +0200

    handles going back to home

--- app/mobile-notice/page.tsx
+++ app/mobile-notice/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import React from "react";
 import { ArrowLeft } from "lucide-react";
 import { signOut } from 'next-auth/react';
 
 export default function MobileNoticePage() {
-  const handleSignOut = (e) => {
+  const handleSignOut = (e: React.FormEvent) => {
     e.preventDefault();
     signOut({ callbackUrl: '/' });
   };
