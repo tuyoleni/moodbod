85cddd6d6b8a370019b9e6081d4de49cc4199825
tuyoleni <simeon.devs@gmail.com>
2025-09-07 14:23:35 +0200

    Remove Navbar and Cta components from AboutPage for a cleaner layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,17 +4,14 @@ import {
   AboutTeam,
   AboutMilestones,
 } from "@/components/about";
-import { Cta, Footer, Navbar } from "@/components/layout";
 
 export default function AboutPage() {
   return (
     <div className="min-h-screen">
-      <Navbar />
       <AboutHero />
       <AboutStory />
       <AboutTeam />
       <AboutMilestones />
-      <Cta />
     </div>
   );
 }
